b44f53ee3e4c478bf9e6738a2cbe838a43f08a2f
Jagmandeep Dhillon <jagmandeep@jagmandeeps-mbp.osustudent.okstate.edu>
2016-10-07 15:04:27 -0500

    Deleted unneccesary code

--- houseeelf-analysis.R
+++ houseeelf-analysis.R
@@ -1,3 +1,2 @@
 #learning git
-data <-read.csv("./data/houseelf_earlength_dna_data.csv",header = FALSE, sep = "", quote = "")
 data1 <- read.csv("./data/houseelf_earlength_dna_data_1.csv",header = FALSE, sep = "", quote = "")
