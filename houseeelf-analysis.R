b1cff1246793b7dbea6931301b177945ee34759e
Jagmandeep Dhillon <jagmandeep@jagmandeeps-mbp.osustudent.okstate.edu>
2016-10-07 15:02:05 -0500

    Renaming of the data file and the changes to the R file

--- houseeelf-analysis.R
+++ houseeelf-analysis.R
@@ -1,2 +1,3 @@
 #learning git
 data <-read.csv("./data/houseelf_earlength_dna_data.csv",header = FALSE, sep = "", quote = "")
+data1 <- read.csv("./data/houseelf_earlength_dna_data_1.csv",header = FALSE, sep = "", quote = "")
